9989578eec81cfcf5bbf81b6bb218d2303a5ace8
Livius Penter <62758571+liviuspenter@users.noreply.github.com>
2023-05-24 13:34:24 -0400

    Create dbGap_aspera_rc-stor15.sh

---
+++ dbGap_aspera_rc-stor15.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# transfer data to dbGap from rc-stor15.dfci.harvard.edu 
+#
+# step1:
+# download version of aspera compatible with glibc on rc-stor15 from https://www.ibm.com/aspera/connect/
+# ibm-aspera-connect-3.11.2.63-linux-g2.12-64.tar.gz
+# 
+# step2: 
+# install aspera connect locally 
+# 
+# step 3: 
+# include ascp executable (.aspera/connect/bin/ascp) in $PATH 
+# 
+# step4: 
+# make sure aspera license is found (needs to be in $HOME/etc/ it seems) 
+# ascp -A 
+#
+# step5: 
+#
+# dbGap upload
+# export ASPERA_SCP_PASS (communicated from dbGap curator)
+export ASPERA_SCP_PASS=XXX
+
+ascp -i $HOME/.aspera/connect/etc/aspera_tokenauth_id_rsa -Q -l 1000m -k 1 fastq asp-dbgap@gap-submit.ncbi.nlm.nih.gov:protected
